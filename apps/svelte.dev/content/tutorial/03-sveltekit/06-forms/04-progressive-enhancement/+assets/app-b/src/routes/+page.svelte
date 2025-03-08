1b64ba3ed13ef17f7de0f4c55875fe56b9f1137f
Romain Crestey <rcrestey@gmail.com>
2025-03-08 16:26:57 +0100

    fr: tutorial/kit/06/04

--- apps/svelte.dev/content/tutorial/03-sveltekit/06-forms/04-progressive-enhancement/+assets/app-b/src/routes/+page.svelte
+++ apps/svelte.dev/content/tutorial/03-sveltekit/06-forms/04-progressive-enhancement/+assets/app-b/src/routes/+page.svelte
@@ -14,7 +14,7 @@
 
 	<form method="POST" action="?/create" use:enhance>
 		<label>
-			add a todo:
+			ajouter une tâche :
 			<input
 				name="description"
 				value={form?.description ?? ''}
@@ -30,7 +30,7 @@
 				<form method="POST" action="?/delete" use:enhance>
 					<input type="hidden" name="id" value={todo.id} />
 					<span>{todo.description}</span>
-					<button aria-label="Mark as complete"></button>
+					<button aria-label="Marquer comme fait"></button>
 				</form>
 			</li>
 		{/each}
